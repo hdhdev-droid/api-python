56a9f14585c6f9965829cc4687c2191fee7f0024
donghwa <donghwa.dev@gmail.com>
2026-02-19 14:34:21 +0900

    fix-port

--- src/config.py
+++ src/config.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PORT = int(os.environ.get("WEB_PORT") or os.environ.get("PORT") or "81")
+# 포트: WAS_PORT > WEB_PORT > PORT > 기본값 81
+PORT = int(os.environ.get("WAS_PORT") or os.environ.get("WEB_PORT") or os.environ.get("PORT") or "81")
 DB_TYPE = (os.environ.get("DB_TYPE") or "").strip().upper() or None
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
